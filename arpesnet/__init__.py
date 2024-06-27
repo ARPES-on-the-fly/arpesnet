9b236ffc26dc6993a31856ea78adb1025c332987
Steinn Ymir Agustsson <agustsson@phys.au.dk>
2024-06-27 22:55:35 +0200

    add clustering notebook

--- arpesnet/__init__.py
+++ arpesnet/__init__.py
@@ -4,11 +4,11 @@ ARPESNet
 
 An autoencoder for ARPES data.
 """
+from . import model, train, transform
+from .core import ModelTrainer, load_trainer
+from .utils import load_config
 
 __version__ = "1.0.0"
 __author__ = "Steinn Ymir Agustsson"
 __email__ = "steinny@gmail.com"
-
-from .core import ModelTrainer
-from .utils import load_config
-from . import transform, train, model
+__all__ = ["ModelTrainer", "load_trainer", "load_config"]
